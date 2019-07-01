0909982a1a02d9de9312aa8efb26d0a340e2a14d
Sebastian Deorowicz <sdeorowicz@gmail.com>
2019-07-01 11:10:43 +0200

    Dodany tryb one2all

--- src/lz-ani.cpp
+++ src/lz-ani.cpp
@@ -19,6 +19,7 @@ using namespace std::chrono;
 
 queue<pair<string, string>> q_files_pairs;
 vector<string> v_files_all2all;
+vector<string> v_files_one2all;
 map<pair<string, string>, CResults> m_results;
 
 mutex mtx_queue;
@@ -30,6 +31,7 @@ int no_threads = 0;
 string input_name;
 string output_name;
 bool is_all2all = false;
+bool is_one2all = false;
 bool buffer_data = false;
 
 int MIN_MATCH_LEN = DEF_MIN_MATCH_LEN;
@@ -49,9 +51,10 @@ int RANGE_TO = DEF_RANGE_TO;
 void load_params(int argc, char** argv);
 void load_tasks_pairs();
 void load_tasks_all2all();
+void load_tasks_one2all();
 void usage();
 void run_pairs_mode();
-void run_all2all_mode();
+void run_one2all_mode();
 
 // ****************************************************************************
 void usage()
@@ -59,6 +62,7 @@ void usage()
 	cerr << "lz-ani [options] <in_list> <output_file>\n";
 	cerr << "Options:\n";
 	cerr << "   -a2a         - turn on all to all mode (default: " << is_all2all << ")\n";
+	cerr << "   -o2a         - turn on one to all mode (default: " << is_one2all << ")\n";
 	cerr << "   -bs          - turn on all sequences buffering  (default: " << buffer_data << ")\n";
 	cerr << "   -t <val>     - no of threads (default: " << no_threads << ")\n";
 	cerr << "   -mml <val>   - min. match length (default: " << MIN_MATCH_LEN << ")\n";
@@ -92,6 +96,13 @@ void load_params(int argc, char** argv)
 		if (par == "-a2a")
 		{
 			is_all2all = true;
+			is_one2all = false;
+			i += 1;
+		}
+		else if (par == "-o2a")
+		{
+			is_one2all = true;
+			is_all2all = false;
 			i += 1;
 		}
 		else if (par == "-bs")
@@ -220,6 +231,12 @@ void load_tasks_all2all()
 	fclose(f);
 }
 
+// ****************************************************************************
+void load_tasks_one2all()
+{
+	load_tasks_all2all();
+	v_files_one2all.swap(v_files_all2all);
+}
 // ****************************************************************************
 void run_pairs_mode()
 {
@@ -493,6 +510,149 @@ void run_all2all_mode()
 }
 
 // ****************************************************************************
+void run_one2all_mode()
+{
+	// Prepare one-2-all pairs
+	for (auto i = 0; i < v_files_one2all.size(); ++i)
+		q_files_pairs.push(make_pair(v_files_one2all[0], v_files_one2all[i]));
+
+	for (int i = 0; i < no_threads; ++i)
+	{
+		v_threads.push_back(thread([&] {
+			CWorker worker;
+
+			while (true)
+			{
+				pair<string, string> task;
+				{
+					lock_guard<mutex> lck(mtx_queue);
+					if (q_files_pairs.empty())
+						return;
+					task = q_files_pairs.front();
+					q_files_pairs.pop();
+				}
+
+				CResults res;
+
+				worker.clear();
+
+				high_resolution_clock::time_point t1 = high_resolution_clock::now();
+				if (!worker.load_data(task.first, task.second))
+				{
+					lock_guard<mutex> lck(mtx_res);
+					cout << task.first << " " << task.second << " - Error!" << endl;
+					continue;
+				}
+
+				// 1 -> 2
+				worker.prepare_kmers();
+				worker.prepare_ht_short();
+				worker.prepare_ht_long();
+
+				worker.parse();
+				//				worker.export_parsing();
+
+				worker.calc_ani(res, 1);
+
+				// 2 -> 1
+				swap(task.first, task.second);
+				worker.swap_data();
+
+				worker.prepare_kmers();
+				worker.prepare_ht_short();
+				worker.prepare_ht_long();
+
+				worker.parse();
+
+				worker.calc_ani(res, 2);
+
+				swap(task.first, task.second);
+
+				res.ani[0] = (res.ani[1] + res.ani[2]) / 2;
+				res.coverage[0] = (res.coverage[1] + res.coverage[2]) / 2;
+
+				high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+				res.time = duration_cast<duration<double>>(t2 - t1).count();
+
+				{
+					lock_guard<mutex> lck(mtx_res);
+					m_results[task] = res;
+
+					cout << task.first << " " << task.second <<
+						" - ANI: " << 100 * res.ani[0] << " (" << 100 * res.ani[1] << " : " << 100 * res.ani[2] << ") " <<
+						"   cov: " << res.coverage[0] << " (" << res.coverage[1] << " : " << res.coverage[2] << ") " <<
+						"    time: " << res.time << endl;
+				}
+			}
+			}));
+	}
+
+	for (auto& x : v_threads)
+		x.join();
+
+
+	FILE* fr1 = fopen((output_name + "1.ani.csv").c_str(), "wb");
+	FILE* fr2 = fopen((output_name + "1.cov.csv").c_str(), "wb");
+	FILE* fr3 = fopen((output_name + "2.ani.csv").c_str(), "wb");
+	FILE* fr4 = fopen((output_name + "2.cov.csv").c_str(), "wb");
+
+	if (!fr1 || !fr2 || !fr3 || !fr4)
+	{
+		cerr << "Cannot create res files\n";
+		exit(0);
+	}
+
+	// 0th -> all
+	fprintf(fr1, ",");
+	fprintf(fr2, ",");
+
+	for (int i = 0; i < (int)v_files_one2all.size(); ++i)
+	{
+		fprintf(fr1, "%s,", v_files_one2all[i].c_str());
+		fprintf(fr2, "%s,", v_files_one2all[i].c_str());
+	}
+
+	fprintf(fr1, "\n");
+	fprintf(fr2, "\n");
+
+	fprintf(fr1, "%s,", v_files_one2all[0].c_str());
+	fprintf(fr2, "%s,", v_files_one2all[0].c_str());
+	
+	for (int i = 0; i < (int)v_files_one2all.size(); ++i)
+	{
+		fprintf(fr1, "%.5f,", m_results[make_pair(v_files_one2all[0], v_files_one2all[i])].ani[1]);
+		fprintf(fr2, "%.5f,", m_results[make_pair(v_files_one2all[0], v_files_one2all[i])].coverage[1]);
+	}
+
+	fprintf(fr1, "\n");
+	fprintf(fr2, "\n");
+
+	// all -> 0th
+	fprintf(fr3, ",");
+	fprintf(fr4, ",");
+	fprintf(fr3, "%s,", v_files_one2all[0].c_str());
+	fprintf(fr4, "%s,", v_files_one2all[0].c_str());
+	fprintf(fr3, "\n");
+	fprintf(fr4, "\n");
+
+	for (int i = 0; i < (int)v_files_one2all.size(); ++i)
+	{
+		fprintf(fr3, "%s,", v_files_one2all[i].c_str());
+		fprintf(fr4, "%s,", v_files_one2all[i].c_str());
+		fprintf(fr3, "%.5f,", m_results[make_pair(v_files_one2all[0], v_files_one2all[i])].ani[2]);
+		fprintf(fr4, "%.5f,", m_results[make_pair(v_files_one2all[0], v_files_one2all[i])].coverage[2]);
+		fprintf(fr3, "\n");
+		fprintf(fr4, "\n");
+	}
+
+	fclose(fr1);
+	fclose(fr2);
+	fclose(fr3);
+	fclose(fr4);
+}
+
+// ****************************************************************************
 int main(int argc, char **argv)
 {
 	load_params(argc, argv);
@@ -508,6 +668,11 @@ int main(int argc, char **argv)
 	{
 		load_tasks_all2all();
 		run_all2all_mode();
+	}
+	else if (is_one2all)
+	{
+		load_tasks_one2all();
+		run_one2all_mode();
 	}
 	else
 	{
